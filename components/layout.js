43aece830b82aabed66a670d4cddfd5bf2dc1962
unknown <diegomartinez_03@outlook.es>
2023-02-19 12:19:23 -0600

    firts commit

---
+++ components/layout.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Navbar from './navbar'
+import Sidenav from './sidenav'
+
+const Layout = (props) => {
+  return (
+    <div>
+        <Head>
+            <title>Next js proyect</title>
+            <link href="https://cdn.jsdelivr.net/npm/daisyui@2.50.1/dist/full.css" rel="stylesheet" type="text/css"/>
+            
+        </Head>
+        <Navbar/>
+        <Sidenav/>
+        
+        
+        <div className='container mx-auto'>
+            {props.children}
+        </div>
+    </div>
+  )
+}
+
+
+
+export default Layout;
