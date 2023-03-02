d34569fe57813c25ad6741598130e68c7d67bab6
unknown <diegomartinez_03@outlook.es>
2023-03-02 07:56:35 -0600

    Deploy vercel

---
+++ pages/mapa.js
@@ -0,0 +1,20 @@
+
+import Head from 'next/head';
+import Layout from '../components/layout'
+import Sidenav from '../components/sidenav'
+
+
+
+const homepage = () => {
+    return (
+
+    <Layout>
+       
+        
+      
+    </Layout>
+    
+    )
+}
+
+export default homepage;
